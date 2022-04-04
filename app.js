0299a2df51729cf9ce56e315e30637eb4e221103
Darshana Perera <darshana.saluka.10@gmail.com>
2022-04-04 06:30:56 +0530

    app.js modified

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./routes/products')
 
-app.use((req, res, next)=>{
-    res.status(200).json({
-        message:'it works'
-    })
-})
+app.use('/products',productRoutes);
 
 module.exports = app;
